cc8a05cc6a4c47c5e1600b4a97bddcbbee4087a9
R3CT <radekrc4@gmail.com>
2026-06-11 12:43:23 +0200

    Fixed Reward message

--- common/src/main/java/com/r3ct/daily/logic/DailyCommands.java
+++ common/src/main/java/com/r3ct/daily/logic/DailyCommands.java
@@ -154,7 +154,7 @@ public class DailyCommands {
 
                         QuestManager.giveOrDrop(player, rewardStack);
 
-                        player.sendSystemMessage(Component.empty().append(QuestManager.getPrefix()).append("- ").withStyle(net.minecraft.ChatFormatting.GRAY).append(
+                        player.sendSystemMessage(Component.empty().append(QuestManager.getPrefix()).append(
                                 Component.translatable("r3ct_daily.message.quests.item_gained", amountComp, translatedItem).withStyle(net.minecraft.ChatFormatting.GREEN)
                         ));
 
